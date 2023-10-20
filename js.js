6084d7eee04eb2aed81597904428ab94954a2ad3
szt2makpet <szt2makpet@vasvari.org>
2023-10-20 08:44:05 +0200

    localStorage

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
 function toggleDarkMode() {
+    const currentMode = localStorage.getItem('mode')
     const body = document.querySelector('body');
     body.classList.toggle('dark-mode');
     body.classList.toggle('light-mode');
+   if (body.classList.contains('light-mode')) {
+    localStorage.setItem('mode', 'light')
+   }else{
+    body.classList.add('dark-mode');
+    localStorage.setItem('mode', 'dark-mode');
+   }
 }
+
